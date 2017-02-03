5d24d851881a1e9a7f04519ea9f90a50377fc159
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-02-03 08:20:57 +0200

    revert back to class component

--- src/components/typing-test.js
+++ src/components/typing-test.js
@@ -3,17 +3,26 @@ import TypingTestContents from './typing-test-contents';
 import TypingTestInput from './typing-test-input';
 import TypingTestTimer from './typing-test-timer';
 
-const TypingTest = (props) => {
-    const textToLines = (lineLength) => {
+class TypingTest extends Component {
+    constructor() {
+        super();
+        this.state = { lines: [""] };
+    }
+
+    componentDidMount() {
+        this.setState({ lines: this.textToLines() });
+    }
+
+    textToLines() {
         let lines = [""]
         let currentLine = 0;
 
-        let words = props.text.split(" ");
+        let words = this.props.text.split(" ");
 
         words.forEach(word => {
             // Create new line if inserting the current word would exceed
             // the max line length
-            if (lines[currentLine].length + word.length > lineLength) {
+            if (lines[currentLine].length + word.length > this.props.lineLength) {
                 lines.push("");
                 currentLine++;
             } else if (lines[currentLine].length > 0)
@@ -23,12 +32,14 @@ const TypingTest = (props) => {
         return lines;
     }
 
-    return(
-        <div>
-            <TypingTestContents text={textToLines(props.lineLength)} />
-            <TypingTestInput onKeyPress={props.onKeyPress} />
-            <TypingTestTimer minutes={props.minutes} seconds={props.seconds} />
-        </div>);
-};
+    render() {
+        return(
+            <div>
+                <TypingTestContents text={textToLines(this.props.lineLength)} />
+                <TypingTestInput onKeyPress={this.props.onKeyPress} />
+                <TypingTestTimer minutes={this.props.minutes} seconds={this.props.seconds} />
+            </div>);
+    }
+}
 
 export default TypingTest;
