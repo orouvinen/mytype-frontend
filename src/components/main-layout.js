912b67b0c4af3995f9846f766c6a790d49f1bb5d
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-02-04 00:51:10 +0200

    store reference is not needed in MainLayout

--- src/components/main-layout.js
+++ src/components/main-layout.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import TypingTestContainer from '../containers/typing-test-container';
-import store from '../store';
 
 const MainLayout = () => (<TypingTestContainer />);
 
