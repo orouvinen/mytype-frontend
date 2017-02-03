a269750a2a81e63d4d5a12c42a6918a1535ee114
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-02-03 07:31:41 +0200

    convert to functional component

--- src/components/typing-test.js
+++ src/components/typing-test.js
@@ -1,27 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TypingTestContents from './typing-test-contents';
 import TypingTestInput from './typing-test-input';
 import TypingTestTimer from './typing-test-timer';
-/*
- * notetoself: this code was much cleaner as a functional component and
- * a simple toLines(text,lineLength) function aside.
- */
-class TypingTest extends Component {
-    constructor() {
-        super();
-        this.textToLines = this.textToLines.bind(this);
-        this.state = { lines: [""] };
-    }
 
-    componentDidMount() {
-        this.setState({ lines: this.textToLines(this.props.lineLength)});
-    }
-
-    textToLines(lineLength) {
+const TypingTest = (props) => {
+    const textToLines = (lineLength) => {
         let lines = [""]
         let currentLine = 0;
 
-        let words = this.props.text.split(" ");
+        let words = props.text.split(" ");
 
         words.forEach(word => {
             // Create new line if inserting the current word would exceed
@@ -36,15 +23,12 @@ class TypingTest extends Component {
         return lines;
     }
 
-    render() {
-        return(
-            <div>
-                <TypingTestContents text={this.state.lines} />
-                <TypingTestInput onKeyPress={this.props.onKeyPress} />
-                <TypingTestTimer minutes={this.props.minutes} seconds={this.props.seconds} />
-            </div>
-        );
-    }
-}
+    return(
+        <div>
+            <TypingTestContents text={textToLines(props.lineLength)} />
+            <TypingTestInput onKeyPress={props.onKeyPress} />
+            <TypingTestTimer minutes={props.minutes} seconds={props.seconds} />
+        </div>);
+};
 
 export default TypingTest;
