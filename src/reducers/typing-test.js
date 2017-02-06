5990b9deae2bd8f208bf9fc93b47786f3b6191de
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-02-06 21:51:51 +0200

    fix indent

--- src/reducers/typing-test.js
+++ src/reducers/typing-test.js
@@ -50,30 +50,30 @@ function advanceWord(state) {
      * TypingTestContents interval function can check if the endTime has been set, and
      * if so, subtract the time difference.
      */
-    }
+  }
 }
 
-    // Convert the text passed in to an array of string, where each
-    // line is no longer than props.lineLength characters.
+// Convert the text passed in to an array of string, where each
+// line is no longer than props.lineLength characters.
 function textToLines(text, lineLength) {
-    let lines = [""]
-    let currentLine = 0;
+  let lines = [""]
+  let currentLine = 0;
 
-    let words = text.split(" ");
+  let words = text.split(" ");
 
-    words.forEach(word => {
+  words.forEach(word => {
     // Create new line if inserting the current word would exceed
     // the max line length
-        if (lines[currentLine].length + word.length > lineLength) {
-            lines.push("");
-            currentLine++;
-        } else if (lines[currentLine].length > 0)
-    // Insert space if this is not the first word on the line
-            lines[currentLine] += " ";
+    if (lines[currentLine].length + word.length > lineLength) {
+      lines.push("");
+      currentLine++;
+    } else if (lines[currentLine].length > 0)
+      // Insert space if this is not the first word on the line
+      lines[currentLine] += " ";
 
-        lines[currentLine] += word;
-    });
-    return lines;
+    lines[currentLine] += word;
+  });
+  return lines;
 }
 
 export default typingTest;
