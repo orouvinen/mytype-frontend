68da545b47797abd0cf08349db6befee20fefba6
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-02-06 21:51:13 +0200

    Fix indent

--- src/actions/action-creators.js
+++ src/actions/action-creators.js
@@ -1,26 +1,26 @@
 export function wordTyped() {
-    return {
-        type: 'TYPING_TEST_WORD_TYPED'  
-    };
+  return {
+    type: 'TYPING_TEST_WORD_TYPED'  
+  };
 };
 
 export function setText(text) {
-    return {
-        type: 'TYPING_TEST_SET_TEXT',
-        text: text,
-    }
+  return {
+    type: 'TYPING_TEST_SET_TEXT',
+    text: text,
+  }
 }
 
 export function typingTestDone(wpm) {
-    return { 
-        type: 'TYPING_TEST_DONE',
-        wpm: wpm, 
-    };
+  return { 
+    type: 'TYPING_TEST_DONE',
+    wpm: wpm, 
+  };
 }
 
 export function startTypingTest(text) {
-    return {
-        type: 'TYPING_TEST_START',
-        text: text,
-    };
+  return {
+    type: 'TYPING_TEST_START',
+    text: text,
+  };
 }
--- src/reducers/typing-test.js
+++ src/reducers/typing-test.js
@@ -1,60 +1,60 @@
 const initialState = {
-    text: [""],
-    currentLineNum: 0,
-    currentWordNum: 0,
-    inProgress: false,
+  text: [""],
+  currentLineNum: 0,
+  currentWordNum: 0,
+  inProgress: false,
 };
 
 function typingTest(state = initialState, action) {
-    switch (action.type) {
-        case 'TYPING_TEST_WORD_TYPED':
-            
-            return advanceWord(state);
+  switch (action.type) {
+    case 'TYPING_TEST_WORD_TYPED':
 
-        case 'TYPING_TEST_SET_TEXT':
-            return {
-                ...state,
-                text: textToLines(action.text, 60),
-            };
-        case 'TYPING_TEST_DONE':
-            return {
-                ...state,
-                inProgress: false,
-            };
-            // break;
-        case 'TYPING_TEST_START':
-            return {
-                ...state,
-                inProgress: true,
-                currentLineNum: 0,
-                currentWordNum: 0,
-            };
-        default:
-            return state;
-    }
+      return advanceWord(state);
+
+    case 'TYPING_TEST_SET_TEXT':
+      return {
+        ...state,
+        text: textToLines(action.text, 60),
+      };
+    case 'TYPING_TEST_DONE':
+      return {
+        ...state,
+        inProgress: false,
+      };
+      // break;
+    case 'TYPING_TEST_START':
+      return {
+        ...state,
+        inProgress: true,
+        currentLineNum: 0,
+        currentWordNum: 0,
+      };
+    default:
+      return state;
+  }
 }
 
 function advanceWord(state) {
-    let newState = {}
-    newState = Object.assign(newState, state);
-    
-    let line = newState.currentLineNum; 
-    let text = newState.text;
-    newState.currentWordNum++;
+  let newState = {}
+  newState = Object.assign(newState, state);
 
-    if (newState.currentWordNum >= text[line].length) {
-        newState.currentWordNum = 0;
-        newState.currentLineNum++;
+  let line = newState.currentLineNum; 
+  let text = newState.text;
+  newState.currentWordNum++;
 
-        /* If the text has been typed, save endTime (timestamp) to state.
-         * TypingTestContents interval function can check if the endTime has been set, and
-         * if so, subtract the time difference.
-         */
+  if (newState.currentWordNum >= text[line].length) {
+    newState.currentWordNum = 0;
+    newState.currentLineNum++;
+
+    /* If the text has been typed, save endTime (timestamp) to state.
+     * TypingTestContents interval function can check if the endTime has been set, and
+     * if so, subtract the time difference.
+     */
     }
 }
 
-// Convert the text passed in to an array of string, where each
-// line is no longer than props.lineLength characters.
+    // Convert the text passed in to an array of string, where each
+    // line is no longer than props.lineLength characters.
 function textToLines(text, lineLength) {
     let lines = [""]
     let currentLine = 0;
@@ -62,13 +62,13 @@ function textToLines(text, lineLength) {
     let words = text.split(" ");
 
     words.forEach(word => {
-        // Create new line if inserting the current word would exceed
-        // the max line length
+    // Create new line if inserting the current word would exceed
+    // the max line length
         if (lines[currentLine].length + word.length > lineLength) {
             lines.push("");
             currentLine++;
         } else if (lines[currentLine].length > 0)
-            // Insert space if this is not the first word on the line
+    // Insert space if this is not the first word on the line
             lines[currentLine] += " ";
 
         lines[currentLine] += word;
