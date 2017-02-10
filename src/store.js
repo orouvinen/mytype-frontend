1ad89e927278090b759bb124463a63f5bd865d0e
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-02-10 22:18:37 +0200

    indent

--- src/store.js
+++ src/store.js
@@ -4,6 +4,6 @@ import typingTest from './reducers/typing-test';
 const rootReducer = combineReducers({ typingTest });
 
 let store = createStore(rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
