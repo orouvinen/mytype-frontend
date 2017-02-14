ce7a0d8a1cbb33e12331148b40841d562b8d1db7
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-02-14 13:14:15 +0200

    streamline time conversion

--- src/helpers/wpm.js
+++ src/helpers/wpm.js
@@ -7,9 +7,10 @@ export function wpm(correctCount, incorrectCount, time) {
   const charCount = correctCount + incorrectCount;
 
   // Convert milliseconds to minutes
-  time /= 1000;
-  time /= 60;
-  
+  //time /= 1000;
+  //time /= 60;
+  time /= 60000;
+
   return Math.max(0, ((charCount / 5) - incorrectCount) / time);
 }
 
