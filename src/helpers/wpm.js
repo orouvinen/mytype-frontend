5a68107d602904c29fe9752f3c5b4e1f024454de
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-02-14 07:50:08 +0200

    wpm calc unchanged

--- src/helpers/wpm.js
+++ src/helpers/wpm.js
@@ -4,7 +4,6 @@
 //  incorrectCount: -"-     incorrectly    -"
 //  time: total time elapsed in milliseconds
 export function wpm(correctCount, incorrectCount, time) {
-  /*
   const charCount = correctCount + incorrectCount;
 
   // Convert milliseconds to minutes
@@ -12,13 +11,6 @@ export function wpm(correctCount, incorrectCount, time) {
   time /= 60;
   
   return Math.max(0, ((charCount / 5) - incorrectCount) / time);
-  */
-  time /= 1000;
-  time /= 60;
-  const totalChars = correctCount + incorrectCount;
-  const grossWPM = (totalChars / 5) / time;
-  const netWPM = grossWPM - (incorrectCount / time);
-  return Math.max(0, netWPM);
 }
 
 export function accuracy(correctCount, incorrectCount) {
