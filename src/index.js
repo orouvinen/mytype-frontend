f40d493d6e5634b89c125c081cc4ce5c726a8747
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-02-22 07:28:15 +0200

    Add simple 404 component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { Route, Router, IndexRoute, browserHistory } from 'react-router';
 import App from './App';
 import MainLayout from './components/main-layout';
+import NotFound from './components/not-found';
 import store from './store';
 import './index.css';
 
@@ -12,6 +13,7 @@ ReactDOM.render(
     <Router history={browserHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={MainLayout} />
+        <Route path="*" component={NotFound} />
       </Route>
     </Router>
   </Provider>,
