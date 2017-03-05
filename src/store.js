7fa3a5487fb2bc042258c5a865c046656c15333b
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-03-05 22:20:36 +0200

    Replace thunk with saga setup

--- src/store.js
+++ src/store.js
@@ -1,12 +1,13 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+// Import reducers
 import typingTest from './reducers/typing-test';
 import auth from './reducers/auth';
 
 const rootReducer = combineReducers({ typingTest, auth });
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const sagaMiddleware = createSagaMiddleware();
 
-let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+let store = createStore(rootReducer, composeEnhancers(applyMiddleware(sagaMiddleware)));
 
 export default store;
