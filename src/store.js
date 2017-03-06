04584786677a697a4b91f469ad834b9b8b8cd58e
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-03-06 21:20:08 +0200

    rename auth reducer to index

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 // Import reducers
 import typingTest from './reducers/typing-test';
-import auth from './reducers/auth';
+import index from './reducers/index';
 import { authSaga } from './sagas/auth-saga';
 
 const rootReducer = combineReducers({ typingTest, auth });
