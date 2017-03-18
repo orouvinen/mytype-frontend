e010b3c35771a40b66a8f1672d8c8e2be21a1eb5
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-03-18 10:57:35 +0200

    add signup functionality (no error handling)

--- src/sagas/sign-up.js
+++ src/sagas/sign-up.js
@@ -1,8 +1,23 @@
 import { take, takeEvery, call, put } from 'redux-saga/effects';
 
 function* signUp(action) {
-  console.log("foo");
-  console.log(action);
+  const { name, email, password } = action;
+  fetch('/api/user', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ name, email, password })
+  }).then(response => {
+    // To get the response JSON:
+    //response.json().then(data => console.log(data));
+
+    // Status in response.status
+    //console.log(response);
+  }).catch(err => {
+    // handle error (no connection / back-end down etc.)
+  });
 }
 
 export function* watchSignUpRequest() {
