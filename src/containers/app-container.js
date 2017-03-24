d73f02686bb3012cd589e1e042aa6d90ce5e078b
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-03-24 22:50:01 +0200

    Explain login link click handler

--- src/containers/app-container.js
+++ src/containers/app-container.js
@@ -6,6 +6,10 @@ import * as authActions from '../actions/auth';
 import store from '../store'; 
 
 class AppContainer extends Component {
+  // A separate onClick handler for link to login page is needed because we want
+  // to clear the authFail flag before rendering <LoginForm> so that error
+  // message that was possibly previously shown in the form are not displayed
+  // when navigating to the Login page.
   loginClicked() {
     store.dispatch(authActions.resetFailState());
     browserHistory.push('/login');
