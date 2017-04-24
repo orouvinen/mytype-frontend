49d1241cce9214bd861cf0106b8c9f8e29750cb3
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-04-24 23:06:52 +0300

    Style competition list

--- src/components/competition-list.js
+++ src/components/competition-list.js
@@ -8,7 +8,15 @@ const competitionControlsWrapper = {
 };
 
 const competitionListWrapper = {
-  background: colors.primary4, 
+  padding: "5px",
+  background: colors.primary4,
+  borderRadius: "1px",
+};
+
+const competitionListRowStyle = {
+  padding: "0.5em",
+  margin: "2px 0 2px 0",
+  borderRadius: "1px",
 };
 
 const CompetitionList = props => (
@@ -17,11 +25,13 @@ const CompetitionList = props => (
       <CompetitionControls onCreateClicked={props.onCreateClicked} />
     </div>
     <div style={competitionListWrapper}>
-      <ul>
         {props.competition.competitions.map((comp, i) => {
-          return(<li key={i}>{comp.createdBy}'s competition</li>)
+          return(<div
+            style={competitionListRowStyle}
+            className="competitionListRow"
+            key={i}>{comp.createdBy}'s competition
+          </div>)
         })}
-      </ul>
     </div>
   </div>
 );
