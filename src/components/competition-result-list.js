bacd085824c3d1ec0968d146ca78104b0428ec32
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-05-20 18:14:37 +0300

    Style results list

--- src/components/competition-result-list.js
+++ src/components/competition-result-list.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import colors from '../colors';
+
+const resultListStyle = {
+  backgroundColor: colors.primary2,
+  marginTop: "5px",
+  marginRight: "5px",
+  padding: "5px",
+  borderRadius: "1px",
+  lineHeight: "2em",
+};
 
 const CompetitionResultList = props => {
   const { competitions, selected } = props.competition;
   const selectedComp = competitions[selected];
+
   /* refactor CompetitionListRow out of this */
-  // if (!selectedComp || !selectedComp.results)
-  //  return null;
 
   if (!selectedComp)
     return <div>No competition selected</div>
-  else return (
-    <div>
+  if (selectedComp.results.length === 0)
+    return <div>No results yet</div>;
+  
+  return (
+    <div style={resultListStyle}>
       <h2>Results</h2>
       {selectedComp.results.map((r, i) => {
         return(
