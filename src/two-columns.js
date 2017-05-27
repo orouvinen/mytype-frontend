36ff97bd836fc1e4b8ca63ec3d3cb168c8bbc5ee
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-05-27 19:05:11 +0300

    Make sidebar wider in 2-column layout

--- src/two-columns.js
+++ src/two-columns.js
@@ -7,10 +7,11 @@ export const sideBar = {
   marginLeft: "35px",
   marginTop: "25px",
   order: "1",
-  flex: "1 6 25%",
+  flex: "1 6 35%",
 };
 
 export const mainContent = {
+  marginTop: "25px",
   order: "2",
-  flex: "3 1 75%",
+  flex: "3 1 65%",
 };
