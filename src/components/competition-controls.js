33504bcecb14f76f470cd047ef665bf83b317bf0
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-06-17 10:11:15 +0300

    Paginate competition list

--- src/components/competition-controls.js
+++ src/components/competition-controls.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 const CompetitionControls = props => (
   <div>
-    <button>&lt;</button>
-    <button>&gt;</button>
-    <button onClick={props.onCreateClicked}>+ Create competition</button>
+    <button className="greenButton" onClick={props.onCreateClicked}>+ Create competition</button>
   </div>
 );
 
