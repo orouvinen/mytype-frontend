bdea78cd0323a44b7f3b25d31152a7aa2edf00f8
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-09-07 08:52:58 +0300

    404 page and link styling

--- src/components/not-found.js
+++ src/components/not-found.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import * as layout from '../three-columns';
 
-const NotFound = () => <div>Page not found</div>;
+const NotFound = () =>
+  <div style={layout.layoutWrapper}>
+    <div style={layout.centerColumn}>
+      <h1 className="headerBar">404</h1>
+      <p>The page you requested doesn't exist.</p>
+    </div>
+  </div>
+
 export default NotFound;
