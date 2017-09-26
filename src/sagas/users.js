db1a7c1014cc3ddaedee6bdbf4ca3b358cb16d1b
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-09-26 23:53:03 +0300

    Remove unused styles and code

--- src/sagas/users.js
+++ src/sagas/users.js
@@ -1,11 +1,10 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import { takeLatest } from 'redux-saga/effects';
 import * as actions from '../actions/user-data';
 import { userDataActions as actionTypes } from '../actions/action-types';
 import { loadUsers } from '../fetch/user';
 import { createApiWorker } from './index';
 
 export function* watchLeaderBoardLoad() {
-  //yield takeLatest(actionTypes.USERDATA_FETCH_USERS_REQUEST, loadLeaderboardUsers);
   yield takeLatest(actionTypes.USERDATA_FETCH_USERS_REQUEST,
     createApiWorker(loadUsers, ['wpm', 'desc'],
       { 
@@ -13,30 +12,4 @@ export function* watchLeaderBoardLoad() {
         "default": status => actions.loadUsersFail(status),
       }
     ));
-}
-
-function* loadLeaderboardUsers() {
-  let response = yield call(loadUsers, 'wpm', 'desc');
-  switch(response.status) {
-    case 200:
-      const body = yield call(() => response.json().then(data => data));
-      yield put(actions.loadUsersSuccess(body.users));
-      break;
-    
-    default:
-      yield put(actions.loadUsersFail());
-      break;
-  }
-}
-
-
-function* apiWorker(fetchFunc, fetchArgs, httpResponseActions) {
-  let response = yield call.apply(fetch, fetchArgs);
-  for(const status in httpResponseActions) {
-    if (httpResponseActions.hasOwnProperty(status)) {
-      const actionCreator = status.value;
-      yield put(actionCreator);
-      return;
-    }
-  }
 }
